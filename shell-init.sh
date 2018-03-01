9d60c3908dfe16bdfac80c0f6b8e441bb1b076d7
Lurton <dlurton@amazon.com>
2018-03-01 15:23:17 -0800

    Source Code Pro font for spacemacs on OSX and some additional util scripts

--- shell-init.sh
+++ shell-init.sh
@@ -4,3 +4,18 @@ PATH=$PATH:~/dotfiles
 
 export OPAMKEEPBUILDDIR=yes
 export OCAMLPARAM="_,bin-annot=1"
+
+# use this to print the last command
+function get-last-cmd() {
+    fc -ln "$1" "$1" | sed '1s/^[[:space:]]*//'
+}
+
+function save-last-cmd() {
+    if [ $# -lt 1 ]; then
+        echo "To what file do you want to save the command as?"
+    else
+        echo "#!/bin/sh -e" > $1
+        get-last-cmd >> $1
+        chmod u+x $1
+    fi
+}
